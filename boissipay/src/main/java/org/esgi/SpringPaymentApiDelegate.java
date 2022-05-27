631a01ff58d97ab35d3db55f66535cc10ac75150
pooetitu <pooetitu@gmail.com>
2022-05-27 15:06:59 +0200

    Add delegate overrides

--- boissipay/src/main/java/org/esgi/SpringPaymentApiDelegate.java
+++ boissipay/src/main/java/org/esgi/SpringPaymentApiDelegate.java
@@ -1,6 +1,34 @@
 package org.esgi;
 
+import java.util.List;
+import java.util.UUID;
 import org.esgi.boissipay.api.PaymentApiDelegate;
+import org.esgi.boissipay.model.Payment;
+import org.esgi.boissipay.model.PaymentResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SpringPaymentApiDelegate implements PaymentApiDelegate {
+
+    @Override
+    public ResponseEntity<PaymentResponse> createPayment(Payment body) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Void> deletePaymentByCheckoutId(UUID checkoutId) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Payment> getPaymentByCheckoutId(UUID checkoutId) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<List<Payment>> getSucceededPayments() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
